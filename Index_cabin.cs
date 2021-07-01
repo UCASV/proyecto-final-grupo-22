52f06e7fafc0e3028ec662f8c0b7d73246ae8d5b
nicolas2321-ux <54415404+nicolas2321-ux@users.noreply.github.com>
2021-06-30 18:27:05 -0600

    Creación de clases

---
+++ Index_cabin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Vacuna.model
+{
+    public class Index_cabin
+    {
+        public int id { set; get; }
+        public string index { set; get; }
+
+        public Index_cabin ()
+        { }
+        public Index_cabin(int id, string index )
+        {
+            this.id = id;
+            this.index = index; 
+        }
+    }
+}
---
+++ InstitutionObjective.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Proyecto_Vacuna.model
+{
+    public class InstitutionObjective
+    {
+        public int id { set; get; }
+        public string Institution { set; get; }
+        
+        public InstitutionObjective ()
+        { }
+        public InstitutionObjective(int id, string institution)
+        {
+            this.id = id;
+            this.Institution = institution;
+        }
+
+    }
+}
