34d2f3b7c74ae68f970ca30b54bd1432fd1ac949
katya Herrera <71042746+katyaH31@users.noreply.github.com>
2021-06-30 19:35:08 -0600

    Subiendo archivos de migraciones

---
+++ Programación Orientada a Objetos/Código Fuente C#/Proyecto_Vacuna1/Migrations/20210628092645_tablas.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Proyecto_Vacuna1.Migrations
+{
+    public partial class tablas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
