9da975d8637f6ad42e3ad21bed997a4801363a61
MarcelaPortillo <71044272+MarcelaPortillo@users.noreply.github.com>
2021-06-30 18:25:20 -0600

    Creación de Cita Clase Vacuna

---
+++ DateVaccine.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#nullable disable
+
+namespace Proyecto_Vacuna.model
+{
+    public class DateVaccine
+    {
+        public int id { set; get; }
+        public string name { set; get; }
+        public virtual Index_cabin Cabin1 { set; get; }
+        
+        public virtual Pacient Pacient { set; get; }
+
+        
+        public DateTime date { set; get; }
+        public virtual TypeVaccine Type { set; get; }
+        public virtual Dosi dosis { set; get; }
+
+        public DateVaccine()
+        { }
+
+        public DateVaccine(Index_cabin cabin, Pacient pacient, DateTime date, TypeVaccine type, Dosi dosis, string name)
+        {
+            this.Cabin1 = cabin;
+            this.Pacient = pacient;
+            this.date = date;
+            this.Type = type;
+            this.dosis = dosis;
+            this.name = name;
+
+        }
+
+       
+
+    }
+}
---
+++ DayVaccine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Proyecto_Vacuna.model
+{
+    public class DayVaccine
+    {
+        public int id { set; get; }
+        public virtual Pacient Pacient { set; get; }
+
+        public virtual Index_cabin Cabin { set; get; }
+
+        public virtual SideEffect Side { set; get; }
+        public virtual TypeVaccine TypeVaccine { set; get; }
+        public virtual Dosi Dosis { set; get; }
+
+        public DayVaccine ()
+
+        { }
+        public DayVaccine(Pacient pacient, Index_cabin cabin, SideEffect side, TypeVaccine type, Dosi dosis)
+        {
+            this.Pacient = pacient;
+            this.Cabin = cabin;
+            this.Side = side;
+            this.TypeVaccine = type;
+            this.Dosis = dosis;
+        }
+    }
+}
